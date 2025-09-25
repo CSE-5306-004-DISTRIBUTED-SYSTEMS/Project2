8d8eeec978929f346a2393e72bed35c6d718ac62
Johncxsong <chenxisong@hotmail.com>
2025-09-25 14:04:51 -0500

    create a database first version

---
+++ microservice_rpc/database/schema.sql
@@ -0,0 +1,34 @@
+
+-- -- Poll table
+-- CREATE TABLE poll (
+--     uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+--     poll_question TEXT NOT NULL,
+--     options TEXT[] NOT NULL,
+--     status VARCHAR(10) CHECK (status IN ('open', 'close')) NOT NULL,
+--     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+-- );
+
+-- -- Vote table
+-- CREATE TABLE vote (
+--     id SERIAL PRIMARY KEY,
+--     user_id UUID NOT NULL,
+--     selected_option TEXT NOT NULL,
+--     vote_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--     poll_uuid UUID REFERENCES poll(uuid) ON DELETE CASCADE
+-- );
+
+
+CREATE TABLE poll (
+    uuid UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    poll_questions TEXT NOT NULL,
+    options TEXT[] NOT NULL,
+    status TEXT CHECK (status IN ('open', 'close')) DEFAULT 'open',
+    create_at_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE vote (
+    userID TEXT NOT NULL,
+    select_options TEXT NOT NULL,
+    vote_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    uuid UUID REFERENCES poll(uuid)
+);
